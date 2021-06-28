a8ea66600b69ed6dd1df946f5eff402fd392297c
Sameer Kulkarni <sameerkulkarni20@outlook.com>
2021-06-28 22:25:10 +0530

    This is the basic spring boot project focused on the pet adoption service

---
+++ petadoption/src/main/java/com/pets/fibicado/petadoption/PetAdoptionApplication.java
@@ -0,0 +1,13 @@
+package com.pets.fibicado.petadoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionApplication.class, args);
+	}
+
+}
---
+++ petadoption/src/test/java/com/pets/fibicado/petadoption/PetAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pets.fibicado.petadoption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
